6138b745eb79f7ccdd400f74a048e098e06d5f19
munsluk <76475432+munsluk@users.noreply.github.com>
2021-04-26 11:49:15 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Hammer
+# Geologist
+Geologist Project
---
+++ stone.js
@@ -0,0 +1,29 @@
+class Stone{
+	constructor(x,y,r)
+	{
+      var options={
+		restitution:0.8,
+		friction:3,
+		density:10
+	}
+
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.rectangle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			pop()
+	}
+
+}
